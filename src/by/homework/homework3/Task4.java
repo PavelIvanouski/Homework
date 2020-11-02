cfb578b51975ac7a4ac9147965fb41cdaa6a0ce6
PavelIvanouski <pavel_85_67@mail.ru>
2020-11-02 11:12:08 +0300

    Homework 3 Task 4

--- src/by/homework/homework3/Task4.java
+++ src/by/homework/homework3/Task4.java
@@ -1,13 +1,30 @@
 package by.homework.homework3;
 
-public class Task4{
-    public static void main(String[] args){
+public class Task4 {
+    public static void main(String[] args) {
         String str = "Just keep swimming. Just keep swimming. Just keep swimming, swimming, swimming. What do we do? We swim, swim.";
         //"Finding Nemo"
-        String[] split = str.split(" ");
-        for (String entry : split) {
-            System.out.println(entry);
+        System.out.println(str);
+        String regex = "[,.?!:;]";
+        String formattedString = str.replaceAll(regex, " ");
+        char marker = '!';
+        String[] split = formattedString.split(" ");
+        for (int i = 0; i < split.length; i++) {
+            if (split[i].isEmpty()) {
+                continue;
+            }
+            String currentWorld = split[i];
+            if (currentWorld.charAt(0) == marker) {
+                continue;
+            }
+            int count = 0;
+            for (int j = 0; j < split.length; j++) {
+                if (split[j].equals(currentWorld)) {
+                    count++;
+                    split[j] = marker + split[j];
+                }
+            }
+            System.out.println("The world \"" + currentWorld + "\" occurs " + count + " time(s).");
         }
-
     }
 }
